f1eb5a9a0aa41902953b86c0d8f461556d0ec5f9
kaio <iceythe@gmail.com>
2023-06-11 01:11:43 +0200

    Add class filter optimization

--- patches/opgroup/filters.py
+++ patches/opgroup/filters.py
@@ -2,6 +2,7 @@
 
 def apply():
     optimize_SelfAttributeFilter_values()
+    optimize_ClassFilter_values()
 
 
 # Optimizes SelfAttributeFilter.values to search within the class scope.
@@ -41,3 +42,62 @@ def optimize_SelfAttributeFilter_values():
 
     SelfAttributeFilter.values = values
 
+
+def optimize_ClassFilter_values():
+    from jedi.inference.flow_analysis import reachability_check, REACHABLE, UNREACHABLE
+    from jedi.inference.value.klass import ClassFilter
+    from itertools import repeat
+    from ..tools import is_basenode, is_namenode, is_funcdef, is_classdef
+
+    cache = {}
+
+    def get_class_name_definitions(self: ClassFilter, scope, context):
+        names = []
+        pool  = scope.children[-1].children[:]
+
+        for n in filter(is_basenode, pool):
+            pool += [n.children[1]] if n.type in {"classdef", "funcdef"} else n.children
+
+        # Get name definitions.
+        for n in filter(is_namenode, pool):
+            if n.get_definition(include_setitem=True):
+                names += [n]
+
+        names = self._filter(names)
+        names = list(map(self.name_class, repeat(context), names))
+        return names
+
+    def values(self: ClassFilter):
+        scope = self._parser_scope
+        names = []
+
+        if is_classdef(scope):
+            context = self.parent_context
+
+            if context.is_stub():
+                if scope not in cache:
+                    cache[scope] = get_class_name_definitions(self, scope, context)
+                return cache[scope]
+            else:
+                pass
+
+            names = get_class_name_definitions(self, scope, context)
+        else:
+            pass  # Should we do anything about non-class scopes?
+        return names
+    
+    # This version differs from the stock ``_check_flows`` by:
+    # - Doesn't sort entries. Not applicable in class filter contexts.
+    # - Doesn't break after first hit. The stock version is was designed to
+    #   process like-named sequences. Here we process *all* names in one go.
+    def _check_flows(self, names):
+        context = self._node_context
+        value_scope = self._parser_scope
+        origin_scope = self._origin_scope
+        for name in names:
+            check = reachability_check(context=context, value_scope=value_scope, node=name, origin_scope=origin_scope)
+            if check is not UNREACHABLE:
+                yield name
+
+    ClassFilter.values = values
+    ClassFilter._check_flows = _check_flows
