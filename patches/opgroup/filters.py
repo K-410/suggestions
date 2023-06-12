ea75ee44b4ee11dfe7910577e93f51634d9b44df
kaio <iceythe@gmail.com>
2023-06-13 01:18:29 +0200

    Add more filter optimizations

--- patches/opgroup/filters.py
+++ patches/opgroup/filters.py
@@ -1,8 +1,15 @@
 # This module implements optimizations for various filter types.
+from jedi.inference.flow_analysis import reachability_check, UNREACHABLE
+from jedi.inference.value.klass import ClassFilter
+from itertools import repeat
+
+from ..tools import is_basenode, is_namenode
+
 
 def apply():
     optimize_SelfAttributeFilter_values()
     optimize_ClassFilter_values()
+    optimize_AnonymousMethodExecutionFilter()
 
 
 # Optimizes SelfAttributeFilter.values to search within the class scope.
@@ -43,61 +50,78 @@ def optimize_SelfAttributeFilter_values():
     SelfAttributeFilter.values = values
 
 
+def get_scope_name_definitions(self: ClassFilter, scope, context):
+    names = []
+    pool  = scope.children[:]
+
+    for n in filter(is_basenode, pool):
+        pool += [n.children[1]] if n.type in {"classdef", "funcdef"} else n.children
+
+    # Get name definitions.
+    for n in filter(is_namenode, pool):
+        if n.get_definition(include_setitem=True):
+            names += [n]
+
+    return list(self._filter(names))
+
+
+# This version differs from the stock ``_check_flows`` by:
+# - Doesn't sort entries. Not applicable in class filter contexts.
+# - Doesn't break after first hit. The stock version is was designed to
+#   process like-named sequences. Here we process *all* names in one go.
+def _check_flows(self, names):
+    context = self._node_context
+    value_scope = self._parser_scope
+    origin_scope = self._origin_scope
+    for name in names:
+        check = reachability_check(context=context, value_scope=value_scope, node=name, origin_scope=origin_scope)
+        if check is not UNREACHABLE:
+            yield name
+
+
 def optimize_ClassFilter_values():
-    from jedi.inference.flow_analysis import reachability_check, REACHABLE, UNREACHABLE
-    from jedi.inference.value.klass import ClassFilter
-    from itertools import repeat
-    from ..tools import is_basenode, is_namenode, is_funcdef, is_classdef
+    from ..tools import is_classdef
 
-    cache = {}
-
-    def get_class_name_definitions(self: ClassFilter, scope, context):
-        names = []
-        pool  = scope.children[-1].children[:]
-
-        for n in filter(is_basenode, pool):
-            pool += [n.children[1]] if n.type in {"classdef", "funcdef"} else n.children
-
-        # Get name definitions.
-        for n in filter(is_namenode, pool):
-            if n.get_definition(include_setitem=True):
-                names += [n]
-
-        names = self._filter(names)
-        names = list(map(self.name_class, repeat(context), names))
-        return names
+    stub_classdef_cache = {}
 
     def values(self: ClassFilter):
-        scope = self._parser_scope
-        names = []
+        context = self.parent_context
+        scope   = self._parser_scope
+        names   = []
 
         if is_classdef(scope):
-            context = self.parent_context
+            # The class suite.
+            scope = scope.children[-1]
 
             if context.is_stub():
-                if scope not in cache:
-                    cache[scope] = get_class_name_definitions(self, scope, context)
-                return cache[scope]
+                if scope not in stub_classdef_cache:
+                    stub_classdef_cache[scope] = get_scope_name_definitions(self, scope, context)
+                names = stub_classdef_cache[scope]
             else:
-                pass
-
-            names = get_class_name_definitions(self, scope, context)
-        else:
-            pass  # Should we do anything about non-class scopes?
-        return names
-    
-    # This version differs from the stock ``_check_flows`` by:
-    # - Doesn't sort entries. Not applicable in class filter contexts.
-    # - Doesn't break after first hit. The stock version is was designed to
-    #   process like-named sequences. Here we process *all* names in one go.
-    def _check_flows(self, names):
-        context = self._node_context
-        value_scope = self._parser_scope
-        origin_scope = self._origin_scope
-        for name in names:
-            check = reachability_check(context=context, value_scope=value_scope, node=name, origin_scope=origin_scope)
-            if check is not UNREACHABLE:
-                yield name
+                names = get_scope_name_definitions(self, scope, context)
+        return list(map(self.name_class, repeat(context), names))
 
     ClassFilter.values = values
     ClassFilter._check_flows = _check_flows
+
+
+def optimize_AnonymousMethodExecutionFilter():
+    from jedi.inference.value.instance import AnonymousMethodExecutionFilter
+    from ..tools import is_funcdef
+
+    cache = {}
+
+    def get(self: AnonymousMethodExecutionFilter, name):
+        # ret = get_orig(self, name)
+        scope = self._parser_scope
+        assert is_funcdef(scope)
+
+        if scope not in cache:
+            cache[scope] = get_scope_name_definitions(self, scope, self.parent_context)
+
+        names = [n for n in cache[scope] if n.value == name]
+        names = self._convert_names(names)
+        return names
+
+    AnonymousMethodExecutionFilter.get = get
+    AnonymousMethodExecutionFilter._check_flows = _check_flows
