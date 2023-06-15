b3d1539769ac19d014233bf6f01f61ed3e5ccabe
kaio <iceythe@gmail.com>
2023-06-15 17:18:48 +0200

    Cleanup: - Imports - Code style / rename - Remove asserts

--- patches/opgroup/stubs.py
+++ patches/opgroup/stubs.py
@@ -1,18 +1,16 @@
 from jedi.inference.gradual.stub_value import StubModuleValue, StubModuleContext, StubFilter
-
-from textension.utils import instanced_default_cache, truthy_noargs
 from jedi.inference.flow_analysis import reachability_check, UNREACHABLE
 
 from itertools import repeat
-from ..tools import is_basenode, is_namenode
+from operator import attrgetter
+from types import ModuleType
 
 from sys import modules as _sys_modules
-from types import ModuleType
-from jedi.inference.value.module import ModuleValue
+from jedi.inference.names import TreeNameDefinition
 from parso.python.tree import Name
 
-from parso.python.tree import Name, BaseNode
-from jedi.inference.names import TreeNameDefinition
+from textension.utils import instanced_default_cache, truthy_noargs
+from ..tools import is_basenode, is_namenode
 
 
 stubfilter_names_cache   = {}
@@ -35,17 +33,14 @@ definition_types = {
 def apply():
     optimize_StubModules()
 
-def _get_imported_module(module_name):
-    if real_module := _sys_modules.get(module_name):
-        if isinstance(real_module, ModuleType):
-            return real_module
-    return None
 
-
-def on_missing_module_names(self: dict, stub):
+def cache_module_names(self: dict, stub_module):
     
-    # If a real module exists and has already been imported, use that.
-    if real_module := _get_imported_module(stub.name.string_name):
+    # If the real counterpart exists for a stub and has already been
+    # imported, we use its keys for names instead of parsing the stub.
+    real_module = _sys_modules.get(stub_module.name.string_name)
+
+    if isinstance(real_module, ModuleType):
         keys = set(real_module.__dict__)
 
     else:
@@ -53,9 +48,9 @@ def on_missing_module_names(self: dict, stub):
         add_key = keys.add
         pool = []
 
-        for f in stub._get_stub_filters(origin_scope=None):
+        for f in stub_module._get_stub_filters(origin_scope=None):
             scope = f._parser_scope
-            pool  += scope.children
+            pool += scope.children
             break
 
         for n in filter(is_basenode, pool):
@@ -64,34 +59,31 @@ def on_missing_module_names(self: dict, stub):
         # Get name definitions.
         for n in filter(is_namenode, pool):
             value = n.value
-            if value in keys:
-                continue
+            if value not in keys:
+                if definition := n.get_definition(include_setitem=True):
+                    if definition.type in {"import_from", "import_name"}:
+                        parent = n.parent
 
-            if definition := n.get_definition(include_setitem=True):
-                if definition.type in {"import_from", "import_name"}:
-                    parent = n.parent
+                        # Non-alias imports are not allowed to be exported.
+                        if parent.type not in {"import_as_name", "dotted_as_name"}:
+                            continue
 
-                    # Non-alias imports are not allowed to be exported.
-                    if parent.type not in {"import_as_name", "dotted_as_name"}:
+                        # Aliased imports allowed only if it matches the name.
+                        name, alias_name = parent.children[::2]
+                        if name.value != alias_name.value:
+                            continue
+
+                    # Exclude names with a single underscore.
+                    if value[0] is "_" != value[:2][-1]:
                         continue
 
-                    # Aliased imports allowed only if it matches the name.
-                    name, alias_name = parent.children[::2]
-                    if name.value != alias_name.value:
-                        continue
+                    add_key(value)
 
-                # There's no guidelines for names with a single underscore. The
-                # trend is to exclude these from stubs, but this means names like
-                # ``sys._getframe`` won't be available.
-                if value[0] is "_" != value[:2][-1]:
-                    continue
-                add_key(value)
-    self[stub] = keys
+    self[stub_module] = keys
     return keys
 
 
-module_names_cache = instanced_default_cache(on_missing_module_names)
-from operator import attrgetter
+module_names_cache = instanced_default_cache(cache_module_names)
 get_start_pos = attrgetter("line", "column")
 
 # TAG: get_parent_scope_fast
@@ -116,6 +108,7 @@ def get_parent_scope_fast(node):
             scope = scope.parent
     return scope
 
+
 class CachedStubFilter(StubFilter):
     _parso_cache_node = None
 
@@ -135,7 +128,7 @@ class CachedStubFilter(StubFilter):
             return self.cache.setdefault(name, list(super().get(name)))
 
     def _check_flows(self, names):
-        for name in sorted(names, key=lambda name: name.start_pos, reverse=True):
+        for name in names:
             check = reachability_check(
                 context=self._node_context,
                 value_scope=self._parser_scope,
@@ -161,24 +154,21 @@ class CachedStubFilter(StubFilter):
 
     def values(self: StubFilter):
         context = self.parent_context
-        assert context.is_stub()
-        value = self.parent_context._value
-        assert isinstance(value, ModuleValue)
-        module_names = module_names_cache[value]
+        value   = context._value
 
         names = []
-        pool = [value.tree_node]
-        assert value.is_stub()
+        pool  = [value.tree_node]
 
         for n in filter(is_basenode, pool):
             pool += [n.children[1]] if n.type in {"classdef", "funcdef"} else n.children
 
+        module_names = module_names_cache[value]
         for n in filter(is_namenode, pool):
             if n.value in module_names:
                 names += [n]
 
         ret = self._filter(names)
-        return list(map(self.name_class, repeat(self.parent_context), ret))
+        return map(self.name_class, repeat(context), ret)
 
 
 class CachedStubModuleContext(StubModuleContext):
